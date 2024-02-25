a1ba5eeee67987f7701eb037ff67dbd1f2a1b0a8
magicspell <magicspellrel@gmai.com>
2024-02-24 20:33:57 -0800

    readme

--- MagSim.py
+++ MagSim.py
@@ -1,8 +1,9 @@
 from typing import Tuple, List
 import math
 
-PERMEABILITY = 1
-MOMENT = math.pow(10, 7)
+POLARITY = 1                # 1: Magnets have same polarity and repell, -1: magnets have opposite and attract
+PERMEABILITY = 1            # Less permeability means less magnetic force.
+MOMENT = math.pow(10, 7)    # Strength of magnetic force, basically does same thing as permeability.
 
 # F = (u/4pi) * (m1*m2/r^2)
 # Returns the displacment the object should receive, in x-y tuple
@@ -19,23 +20,19 @@ def apply_mag(object : Tuple[int], other : Tuple[int]) -> Tuple[int]:
     # Find the x-y components of F
     x_mul = (object[0] - other[0])/dist
     y_mul = (object[1] - other[1])/dist
-    return (F*x_mul, F*y_mul)
+    return (POLARITY*F*x_mul, POLARITY*F*y_mul)
 
 # Returns list of objects after applying magnetic forces onece
 def update_objects(objects : List[Tuple[int]]) -> List[Tuple[int]]:
     new_objs = []
 
-    o_i = 0
     for o in objects:
         to_move = [0, 0]
 
-        other_i = 0
         for other in objects:
-            if o_i != other_i:
+            if o != other:
                 new_move = apply_mag(o, other)
                 to_move[0] += new_move[0]
                 to_move[1] += new_move[1]
-            other_i += 1
         new_objs.append((o[0] + to_move[0], o[1] + to_move[1]))
-        o_i += 1
     return new_objs
---
+++ README.md
@@ -0,0 +1,4 @@
+# MagSim
+### A magnetic force simulator
+
+A simple library to simulate multiple 2d objects with positive magnetic forces acting on eachother.
